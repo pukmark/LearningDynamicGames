91c05420802d04044136b65bb910b201ecd01d39
pukmark <pkmark@berkeley.edu>
2026-07-22 16:37:33 +0300

    new initial9

--- LDG_Simulation.py
+++ LDG_Simulation.py
@@ -31,7 +31,7 @@ learned_data_path = "LearnedData.pkl"
 x1f = np.array([player_state(1.0, 1.5, dynamics_type=dynamics_type)])
 x2f = np.array([player_state(-1.0, 1.5, dynamics_type=dynamics_type)])
 max_workers = max(1, int(os.cpu_count() * 0.35))
-max_workers = 1
+# max_workers = 1
         
 
 if __name__ == '__main__':
