576765f35b67b2f65a9882b3f0ca9aa3e5308833
pukmark <pkmark@berkeley.edu>
2026-08-19 14:18:27 -0700

    backup controller

--- LDG_Simulation.py
+++ LDG_Simulation.py
@@ -35,7 +35,7 @@ terminal_constraint_mode = "sampled_points" # {"convex_hull", "sampled_points"}
 # joint control output is applied to both players.
 cooperative_mode = True
 bargaining_gammas = np.array([0.45, 0.5, 0.55])
-bargaining_gammas = np.array([0.5])
+# bargaining_gammas = np.array([0.5])
 # Optional fixed (b1_t, b2_t) costs-to-go. When this is None, iterations after
 # the bootstrap use the previous completed totals minus costs executed so far.
 disagreement_costs = None
